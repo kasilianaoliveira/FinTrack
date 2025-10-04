f00c95a9affc15aa0fb0d09b8705dcb611bcf5fd
kasilianaoliveira <kasilianaoliveira@gmail.com>
2025-10-04 19:37:08 -0300

    feat: add lint configuration

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', () => {
+	return 'Hello World!';
+});
+
+app
+	.listen({
+		port: 3333,
+		host: '0.0.0.0',
+	})
+	.then(() => {
+		console.log('Server running on http://localhost:3333');
+	});
